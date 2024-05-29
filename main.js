e8f33b91181e6f307b0d0854ade67ae118efe756
강은찬 <kangeunchan080310@gmail.com>
2024-05-29 22:11:19 +0900

    main.js에 checkRepository 기능 추가

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ const checkRepository = require('./repositoryChecker');
 // console.log(fetchRepositoryNames);
 // check import modules from repositoryFetcher.js
 
+
 async function main() {
+
+    let RepositoryStatus;
+
     try {
         const repositoriesNames = await fetchRepositoryNames();
-        console.log(checkRepository(repositoriesNames));
+        console.log();
+        RepositoryStatus = checkRepository(repositoriesNames);
+        console.log(RepositoryStatus);
     } catch (err) {
         console.error('Error fetching repository names:', err);
     }
